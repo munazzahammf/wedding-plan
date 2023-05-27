7640d2787502dfe9e78e16134c176754228d11fa
Mohamed Mushraf <mushrafmim@gmail.com>
2023-05-27 23:55:21 +0530

    loginUser method  added to User Controller.

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -1,29 +1,39 @@
 <?php
-    require_once 'database.php';
-    require_once 'models/User.php';
+require_once 'models/User.php';
 
-    class UserController
+class UserController
+{
+    private $db;
+    private User $userModel;
+
+    public function __construct($db)
     {
-        private $db;
-        private User $userModel;
-        
-        public function __construct($db)
-        {
-            $this->db = $db;
-            $this->userModel = new User($this->db);
-        }
-        
-        public function registerUser($firstname, $lastname, $username, $password, $email)
-        {
-            $existingUser = $this->userModel->getUserByUsernameAndEmail($email, $username);
-            
-            if ($existingUser) {
-                return "User already exists.";
-            }
-            
-            $this->userModel->createUser($firstname, $lastname, $username, $password, $email);
-            
-            return "Success.!";
-        }
+        $this->db = $db;
+        $this->userModel = new User($this->db);
     }
-?>
+
+    public function loginUser($email, $password)
+    {
+        $user = $this->userModel->getUserByEmail($email);
+
+        if ($user && $user['password'] == $password) {
+            return $user;
+        }
+
+        return false;
+    }
+
+    public function registerUser($firstname, $lastname, $username, $password, $email)
+    {
+        $existingUser = $this->userModel->getUserByUsernameAndEmail($email, $username);
+
+
+        if ($existingUser) {
+            return "User already exists.";
+        }
+
+        $this->userModel->createUser($firstname, $lastname, $username, $password, $email);
+
+        return "Success.!";
+    }
+}
