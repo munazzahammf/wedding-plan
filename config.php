b190c6dc220e6181e07f7e174e5cb0c414190042
Mohamed Mushraf <mushrafmim@gmail.com>
2023-05-27 17:41:26 +0530

    config.php deleted and database.php will handle task.

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $host="localhost";
-    $dbUsername="root";
-    $dbPort=4200;
-    $dbPassword="";
-    $dbName="dreamday";
-    $conn=mysqli_connect($host,$dbUsername,$dbPassword,$dbName);
-?>
