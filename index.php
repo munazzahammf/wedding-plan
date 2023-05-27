610d84c60228e8041f9cec430bc75625b9f2c6f4
Mohamed Mushraf <mushrafmim@gmail.com>
2023-05-27 10:28:36 +0530

    bootstrap icon and login dropdown added.

--- index.php
+++ index.php
@@ -1,8 +1,3 @@
-<?php
-
-?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -19,6 +14,9 @@
 
    <!-- bootstrap link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.1/css/bootstrap.min.css">
+
+   <!-- bootstrap icons -->
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 
    <!-- bootstrap edited classes -->
    <link rel="stylesheet" href="css/bootstrap.css">
@@ -79,9 +77,27 @@
                   <li class="nav-item">
                      <a class="nav-link">Contact Us</a>
                   </li>
-                  <li class="nav-item">
-                     <a href="#contact" class="link-btn">Login</a>
-                  </li>
+                  <?php
+
+                  $loggedIn = true;
+
+                  if ($loggedIn) {
+                     // echo '<i class="bi bi-person-circle" style="font-size:2.5rem;"></i>';
+                     echo '        <li class="nav-item dropdown">
+          <a class="nav-link" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <i class="bi bi-person-circle"></i>
+          </a>
+          <ul class="dropdown-menu dropdown-menu-right" style="right:0;">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">LOG OUT</a></li>
+          </ul>
+        </li>';
+                  } else {
+                     echo '<a href="#contact" class="link-btn">Login</a>';
+                  }
+                  ?>
                </ul>
             </div>
 
@@ -278,6 +294,7 @@
 
    <!-- java script link -->
    <script src="java-script/cal.js"></script>
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </body>
 
 </html>
