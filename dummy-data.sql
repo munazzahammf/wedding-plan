14d96b6f30a3c5ecac7cfff10fd661cade717b90
Mohamed Mushraf <mushrafmim@gmail.com>
2023-09-01 08:13:34 +0530

    sql updated.

--- dummy-data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `user`(firstname, lastname, username, password, email)
-VALUES
-(Fathima, Munazzaha, munazzahammf, 1234, munazzahammf@gmail.com),
-(Fathima, Ifla, ifla, 1234, ifla@gmail.com)
--- mywed.sql
+++ mywed.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Aug 31, 2023 at 08:14 PM
+-- Generation Time: Sep 01, 2023 at 04:41 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -31,17 +31,18 @@ CREATE TABLE `review` (
   `id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
   `review` varchar(1000) DEFAULT NULL,
-  `email` varchar(100) DEFAULT NULL
+  `email` varchar(100) DEFAULT NULL,
+  `visibility` tinyint(1) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `review`
 --
 
-INSERT INTO `review` (`id`, `name`, `review`, `email`) VALUES
-(3, 'Emily and James', 'Working with \"DreamDay\" was an absolute pleasure. They took care of every detail and made our wedding day stress-free and magical. The team\'s professionalism and attention to detail exceeded our expectations. Highly recommended!', 'emily@gmail.com'),
-(4, 'Hardik Ali', 'We cannot thank \"DreamDay\" enough for their exceptional service. From the initial planning stages to the actual day, they were dedicated and attentive. Our guests were blown away by the stunning decorations and seamless coordination. It was the wedding of our dreams!', 'hardikali@gmail.com'),
-(6, 'Jessica and David', 'We were impressed by the expertise and professionalism of \"DreamDay\". The team guided us throughout the planning process, offering valuable insights and recommendations. On the day of our wedding, everything ran smoothly, allowing us to fully enjoy our celebration. Thank you!', 'jessi@yahoo.com');
+INSERT INTO `review` (`id`, `name`, `review`, `email`, `visibility`) VALUES
+(3, 'Emily and James', 'Working with \"DreamDay\" was an absolute pleasure. They took care of every detail and made our wedding day stress-free and magical. The team\'s professionalism and attention to detail exceeded our expectations. Highly recommended!', 'emily@gmail.com', 1),
+(4, 'Hardik Ali', 'We cannot thank \"DreamDay\" enough for their exceptional service. From the initial planning stages to the actual day, they were dedicated and attentive. Our guests were blown away by the stunning decorations and seamless coordination. It was the wedding of our dreams!', 'hardikali@gmail.com', 0),
+(6, 'Jessica and David', 'We were impressed by the expertise and professionalism of \"DreamDay\". The team guided us throughout the planning process, offering valuable insights and recommendations. On the day of our wedding, everything ran smoothly, allowing us to fully enjoy our celebration. Thank you!', 'jessi@yahoo.com', 0);
 
 -- --------------------------------------------------------
 
@@ -63,11 +64,7 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `firstname`, `lastname`, `username`, `password`, `email`) VALUES
-(1, 'Fathima', 'Munazzaha', 'munazzahammf', '1234', 'munazzahammf@gmail.com'),
-(13, 'Mohamed', 'Mushraf', 'user', '123', 'mushrafmim@gmail.com'),
-(15, 'Mohamed', 'Mushraf', 'hi', '123', 'hi@gmail.com'),
-(16, 'Mohamed', 'Mushraf', '1234', '1234', 'hello@gmail.com'),
-(17, 'Mohamed', 'Mushraf', 'sfsd', 'sss', 'sfdf@gmail.com');
+(1, 'Fathima', 'Munazzaha', 'munazzahammf', '1234', 'munazzahammf@gmail.com');
 
 --
 -- Indexes for dumped tables
